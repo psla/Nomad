3fd607a2e5dc45f7f0e8718f5bb6eef6fb365603
Piotr Slatala <piotr@sepio.pl>
2010-05-17 12:40:58 +0200

    FIX: Removed failing test

--- Source/TestsShared.Tests/src/FakeTests.cs
+++ Source/TestsShared.Tests/src/FakeTests.cs
@@ -6,13 +6,6 @@ namespace TestsShared.Tests
     public class FakeTests
     {
         [Test]
-        public void FailedTest ()
-        {
-            Assert.Fail ("Failed test failed");
-        }
-
-
-        [Test]
         public void Passtest ()
         {
             Assert.Pass ("Passed test passed");
