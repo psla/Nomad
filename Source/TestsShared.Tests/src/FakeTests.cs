d29567d0e21d5dc994b97553e1a3125726d67636
Piotr Slatala <piotr@sepio.pl>
2010-05-17 12:51:31 +0200

    ADD: Build after push test

--- Source/TestsShared.Tests/src/FakeTests.cs
+++ Source/TestsShared.Tests/src/FakeTests.cs
@@ -5,10 +5,22 @@ namespace TestsShared.Tests
     [TestFixture]
     public class FakeTests
     {
+        ///<summary>
+        /// Automatic pass test
+        ///</summary>
         [Test]
         public void Passtest ()
         {
             Assert.Pass ("Passed test passed");
+        }
+
+        ///<summary>
+        /// If build runs after push
+        ///</summary>
+        [Test]
+        public void PostReceiveTest()
+        {
+            Assert.Pass("Checking if build runs after push");
         }
     }
 }
