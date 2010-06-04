fe7b059cd305a1d10cda1b5f767196b6d012c7aa
Maciej Kowalewski <maciej@maciejkowalewski.com>
2010-06-04 23:21:09 +0200

    Documentation for RegionFactory and IRegionFactory

--- Source/Nomad/Regions/IRegionFactory.cs
+++ Source/Nomad/Regions/IRegionFactory.cs
@@ -1,9 +1,20 @@
+using System;
 using System.Windows;
 
 namespace Nomad.Regions
 {
+    /// <summary>
+    ///     Defines methods required to create a region and attach it to a view
+    /// </summary>
     public interface IRegionFactory
     {
+        /// <summary>
+        ///     Creates and attaches new region to the specified <paramref name="view"/>
+        /// </summary>
+        /// <param name="view">View to create new region for</param>
+        /// <returns>Initialized and attached <see cref="IRegion"/> object</returns>
+        /// <exception cref="InvalidOperationException">When a new region cannot be instantiated, beause view's type is not supported by this factory</exception>
+        /// <exception cref="ArgumentNullException">When <paramref name="view"/> is null</exception>
         IRegion CreateRegion(DependencyObject view);
     }
 }
--- Source/Nomad/Regions/RegionFactory.cs
+++ Source/Nomad/Regions/RegionFactory.cs
@@ -5,23 +5,60 @@ using System.Linq;
 namespace Nomad.Regions
 {
     /// <summary>
-    ///     Default implementation of <see cref="IRegionFactory"/>
+    ///     Default implementation of <see cref="IRegionFactory"/>.
     /// </summary>
+    /// <remarks>
+    /// <para>
+    ///     <see cref="RegionFactory"/> creates regions using a collection
+    ///     of <see cref="IRegionAdapter"/>. Every <see cref="IRegionAdapter"/>
+    ///     declares what type of views it can adapt. The <see cref="RegionFactory"/>
+    ///     then uses the most appropriate <see cref="IRegionAdapter"/> to create
+    ///     a region that will be attached to a view.
+    /// </para>
+    /// <para>
+    ///     When looking for appropriate adapter, region factory begins by looking for adapter
+    ///     that's supported type is type of view. If no such adapter is found, an adapter
+    ///     supporting view's base type is used. Type hierarchy is then traversed up until
+    ///     an adapter is found or all types have been considered. If no adapter is found by 
+    ///     that time, an exception is thrown.
+    /// </para>
+    /// <para>
+    ///     <see cref="IRegionAdapter">Region adapters</see> have to be known during creation
+    ///     of the <see cref="RegionFactory">region factory</see>. At the moment, there is no
+    ///     support for adding region adapters after the factory has been created.
+    /// </para>
+    /// </remarks>
     public class RegionFactory : IRegionFactory
     {
         private readonly IRegionAdapter[] _regionAdapters;
 
 
+        /// <summary>
+        /// Initializes new instance of <see cref="RegionFactory"/>.
+        /// </summary>
+        /// <param name="regionAdapters">A collection of region adapters that will be used by this <see cref="RegionFactory"/>.</param>
+        /// <exception cref="ArgumentNullException">When <paramref name="regionAdapters"/> is null.</exception>
         public RegionFactory(IRegionAdapter[] regionAdapters)
         {
-            _regionAdapters = regionAdapters;
+            if (regionAdapters == null) throw new ArgumentNullException("regionAdapters");
+
+            _regionAdapters = regionAdapters.ToArray();
         }
 
 
+        /// <summary>
+        ///     Creates and attaches new region to the specified <paramref name="view"/>
+        /// </summary>
+        /// <param name="view">View to create new region for</param>
+        /// <returns>Initialized and attached <see cref="IRegion"/> object</returns>
+        /// <exception cref="InvalidOperationException">When a new region cannot be instantiated, beause view's type is not supported by this factory</exception>
+        /// <exception cref="ArgumentNullException">When <paramref name="view"/> is null</exception>
         public IRegion CreateRegion(DependencyObject view)
         {
+            if (view == null) throw new ArgumentNullException("view");
+
             var type = view.GetType();
-            while(type != null)
+            while (type != null)
             {
                 var adapter = FindAdapterFor(type);
                 if (adapter != null)
@@ -30,7 +67,8 @@ namespace Nomad.Regions
                 type = type.BaseType;
             }
 
-            throw new InvalidOperationException("No adapter has been found for view of type " + view.GetType().Name);
+            throw new InvalidOperationException("No adapter has been found for view of type " +
+                                                view.GetType().Name);
         }
 
 
