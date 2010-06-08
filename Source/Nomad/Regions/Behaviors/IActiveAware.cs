6f3bed039dd30fd2f2f4d5a0bc63161fae7e942e
Maciej Kowalewski <maciej@maciejkowalewski.com>
2010-06-08 21:32:49 +0200

    Documented IActiveAware interface

--- Source/Nomad/Regions/Behaviors/IActiveAware.cs
+++ Source/Nomad/Regions/Behaviors/IActiveAware.cs
@@ -1,10 +1,17 @@
 namespace Nomad.Regions.Behaviors
 {
     /// <summary>
-    /// Specifies that view should be notified when it becomes active
+    ///     Specifies that view should be notified when it becomes active.
     /// </summary>
     public interface IActiveAware
     {
+        /// <summary>
+        ///     Called when view is activated or deactivated.
+        /// </summary>
+        /// <remarks>
+        ///     This method must not throw any exceptions.
+        /// </remarks>
+        /// <param name="isActive">True when view is active, false otherwise</param>
         void SetIsActive(bool isActive);
     }
 }
