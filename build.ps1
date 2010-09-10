b7dc67861d05e51ae7858831fc45e533ea2a67ab
piotr <Piotr@BlueSpin.(none)>
2010-09-10 15:56:31 +0200

    Added task for generating documentation based od Enviormental Varibles settings with Sandcastle and hhc

--- build.ps1
+++ build.ps1
@@ -1,6 +1,7 @@
 # Product information
 properties {
 	$version = "0.0.0.0"
+	$framework_version = "3.5"
 	$product = "Nomad"
     $company = "Miko³aj Dobski, Piotr Jessa, Maciej Kowalewski, Piotr Œlata³a"
     $copyright = "(C) 2010 Miko³aj Dobski, Piotr Jessa, Maciej Kowalewski, Piotr Œlata³a"
@@ -13,6 +14,7 @@ properties {
 	$build_dir = "$base_dir\Build"
 	$source_dir = "$base_dir\Source"
 	$release_dir = "$base_dir\Release"
+	$documentation_dir = "$base_dir\Documentation"
 	
 	$sln_file = "$source_dir\$product.sln"
 }
@@ -68,11 +70,13 @@ task Logo -depends GetVersion -description "Displays build header" -action {
 task Clean {
     Remove-Item -Recurse -Force $release_dir -ErrorAction SilentlyContinue
     Remove-Item -Recurse -Force $build_dir -ErrorAction SilentlyContinue
+	Remove-Item -Recurse -Force $documentation_dir -ErrorAction SilentlyContinue
 }
  
 task Init -depends Clean, GetProjects {
     New-Item $release_dir -ItemType directory | Out-Null
     New-Item $build_dir -ItemType directory | Out-Null
+	New-Item $documentation_dir -ItemType directory | Out-Null
 	New-Item $build_dir\Modules -ItemType directory | Out-Null
     
     # generate assembly infos
@@ -115,12 +119,96 @@ task UnitTest -depends Compile {
     }
 }
 
+task Documentation -depends Compile, GetProjects -description "Provideds automated documentation" {
+	
+	#Prepare EVN Varibles ( We expect no Windows XP x64 or Win2003 x64 )
+	if( (Get-WmiObject -computername $env:computername -class Win32_OperatingSystem ).OSArchitecture -eq "64-bit" )
+	{
+		echo "Setting 64 bit paths"
+		#$env:DxRoot = "$env:ProgramFiles(x86)" + "\Sandcastle"
+		#$DxRoot = "C:\Program Files (x86)\Sandcastle"
+		#[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files (x86)\Sandcastle\ProductionTools","Process")
+		#[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files (x86)\Sandcastle\ProductionTransforms","Process")
+		#[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files (x86)\HTML Help Workshop","Process")
+		
+		#$env:Path = $env:Path + ";C:\Program Files (x86)\HTML Help Workshop"
+		
+		#Prepare Command for execution
+		#$documentation_command = "C:\Program Files (x86)\Sandcastle\ProductionTools\scbuild.ps1' -DxRoot '$DxRoot' -BuildChm -TempDir $temp_dir -framework $framework_version -name $documentation_dir/$product -sources "
+		
+		#$documentation_command = "C:\Program Files (x86)\Sandcastle\ProductionTools\scbuild.ps1' -DxRoot '$DxRoot' -BuildChm -TempDir $temp_dir -framework $framework_version -name $documentation_dir/$product -sources "
+		#$documentation_command = "'" + $documentation_command
+	}
+	else
+	{
+		echo "Setting 32 bit paths"
+		#$env:DxRoot = "$env:ProgramFiles\Sandcastle"
+		#$env:Path = $env:Path + ";$env:ProgramFiles\Sandcastle\ProductionTools"
+		#$env:Path = $env:Path + ";$env:ProgramFiles\HTML Help Workshop"
+	}
+	
+		$documentation_command = "scbuild -BuildChm  -framework $framework_version -name $documentation_dir/$product -sources "
+	
+	echo "Getting list of projects with documentation: "
+	
+	#Get Projects builded by Nomad Team and provide them with documentation.
+	if($script:projects)
+	{
+		foreach($project in $script:projects) 
+		{
+			#Skip the test projects ??? 
+			if( $($project.Name) -match ".*test.*" )
+			{
+				continue
+			}
+			
+			$project_file = cat "$source_dir\$($project.Name)\$($project.Name).csproj"
+			
+			$regex = [regex]"<OutputType>([A-Za-z]+)</OutputType>"
+			$value =  $regex.Matches($project_file).Item(0).Groups[1].Value
+			
+			if ( $value -eq "Library" )
+			{
+				$sufix = "dll"
+			}
+			elseif ( $value -eq "WinExe" )
+			{	
+				$sufix = "exe"
+			}
+			
+			echo "$($project.Name).$sufix with $($project.Name).XML"
+			
+			$documentation_command += "$build_dir/$($project.Name).$sufix, $build_dir/$($project.Name).XML"
+			$documentation_command += ","
+		}
+	}
+	
+	#Remove the last element and save all verbose information from Sandcastle to log file.
+	$documentation_command  = $documentation_command.Substring(0, $documentation_command.Length - 1 )
+	#$documentation_command += " > $temp_dir\Documentation.log"
+	echo $documentation_command
+	#Perform this task in big try ... catch block, beacuse of failure rate during documentation, should not stop the entire buid process
+	echo "Begging generating documentation... " 
+	
+	try
+	{
+		Invoke-Expression -Command $documentation_command 
+		#Exec { & $documentation_command }
+		#& $documentation_command 
+	}
+	catch [Exception]
+	{
+		"Error !!! : $_" 
+	}
+}
+
 task CompileSimplestModules -depends Compile {
 	Push-Location $build_dir\Modules
 	Exec { & csc.exe /out:SimplestModulePossible1.dll /target:library $source_dir\SimplestModulePossible.cs /reference:$build_dir/Nomad.dll /r:$build_dir/Nomad.Tests.dll}
 	Exec { & csc.exe /out:SimplestModulePossible2.dll /target:library $source_dir\SimplestModulePossible.cs /reference:$build_dir/Nomad.dll /r:$build_dir/Nomad.Tests.dll}
 	Pop-Location
 }
+
 
 task Release -depends UnitTest,CompileSimplestModules {
 	 
