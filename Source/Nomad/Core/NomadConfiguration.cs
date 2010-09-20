d71c8448219570935e56cf79f1ff1f3e2c0317e6
Mikolaj Dobski <ankhazam@plusnet.pl>
2010-09-20 15:49:58 +0200

    Introduced basic NomadKernel class that will combine all core modules. It utilizes NomadConfiguration class as its setup.
    Also removed White.Nunit reference to a nonexisting library in TestsShared.

---
+++ Source/Nomad/Core/NomadConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Nomad.Core
+{
+    /// <summary>
+    /// Contains all informations concerning Nomad configuration. Also provides default configuration.
+    /// </summary>
+    public class NomadConfiguration
+    {
+        /// <summary>
+        /// Contains string based configuration dictionary.
+        /// </summary>
+        public IDictionary<string, string> SettingsDictionary;
+
+        //TODO: ADD rest of configuration-dependent classes/interfaces.
+
+        /// <summary>
+        /// Provides default and user-modifiable configuration for Nomad framework.
+        /// </summary>
+        public static NomadConfiguration Default
+        {
+            get
+            {
+                return new NomadConfiguration
+                           {
+                               SettingsDictionary = new Dictionary<string, string>
+                                                        {
+                                                            {"setting1", "value1"}
+                                                        }
+                           };
+            }
+        }
+    }
+}
---
+++ Source/Nomad/Core/NomadKernel.cs
@@ -0,0 +1,28 @@
+﻿namespace Nomad.Core
+{
+    /// <summary>
+    /// Nomad's entry point. Presents Nomad's features to the developer.
+    /// </summary>
+    public class NomadKernel
+    {
+        /// <summary>
+        /// Initializes new instance of the <see cref="NomadKernel"/> class.
+        /// </summary>
+        /// <param name="nomadConfiguration">
+        /// Configuration used to initialize kernel modules.
+        /// If this parameter is NULL, then Nomad uses default configuration available as <see cref="NomadConfiguration.Default"/> property.
+        /// </param>
+        public NomadKernel(NomadConfiguration nomadConfiguration)
+        {
+            NomadConfiguration tempKernelConfiguration;
+            if (nomadConfiguration == null)
+            {
+                tempKernelConfiguration = NomadConfiguration.Default;
+            }
+            else
+            {
+                tempKernelConfiguration = nomadConfiguration;
+            }
+        }
+    }
+}
