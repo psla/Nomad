2b445180fcf86599822f5acb0660d4a20e8b8de8
Piotr Slatala <piotr@sepio.pl>
2010-10-06 19:21:10 +0200

    Fixes in build.ps1

--- build.ps1
+++ build.ps1
@@ -95,7 +95,7 @@ function remote_tests([string] $tests_category)
 		$pipe.Connect()
 		$sw = New-Object System.IO.StreamWriter($pipe)
 		$sw.AutoFlush = $TRUE;
-		$sw.WriteLine("'$lib_dir\NUnit\nunit-console.exe'");
+		$sw.WriteLine("$lib_dir\NUnit\nunit-console.exe");
 		$sw.WriteLine("/nologo /include:$tests_category '$test_assembly' /xml='$results_path'");
 		$sw.Flush();
 		$sr = New-Object System.IO.StreamReader($pipe)
