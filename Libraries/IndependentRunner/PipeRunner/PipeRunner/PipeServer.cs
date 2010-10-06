b77b402fd3e901cfa38e192e6d7ad31a798720fb
Piotr Slatala <piotr@sepio.pl>
2010-10-06 19:07:26 +0200

    Base pipe runner

---
+++ Libraries/IndependentRunner/PipeRunner/PipeRunner/PipeServer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+
+namespace PipeRunner
+{
+    public class PipeServer
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                using (var pipeServer =
+                    new NamedPipeServerStream("runnerPipe", PipeDirection.InOut))
+                {
+                    Console.WriteLine("NamedPipeServerStream object created.");
+
+                    Console.WriteLine("Waiting for client");
+
+                    // Wait for a client to connect
+                    pipeServer.WaitForConnection();
+
+                    Console.WriteLine("Client connected.");
+
+                    StreamWriter sw = null;
+                    StreamReader sr = null;
+                    try
+                    {
+                        // Read user input and send that to the client process.
+
+                        string commandToExecute = "";
+                        string parameters = "";
+                        int exitCode = 1;
+                        sr = new StreamReader(pipeServer);
+                        commandToExecute = sr.ReadLine();
+                        parameters = sr.ReadLine();
+
+                        Console.WriteLine("Executing provided command {0} {1}", commandToExecute, parameters);
+
+                        using (Process process = Process.Start(commandToExecute, parameters))
+                        {
+                            process.WaitForExit();
+                            exitCode = process.ExitCode;
+                            Console.WriteLine("Exit code returned from application: {0}", exitCode);
+                        }
+
+                        Console.WriteLine("Finishing");
+                        sw = new StreamWriter(pipeServer);
+
+                        sw.AutoFlush = true;
+                        sw.WriteLine(exitCode);
+                    }
+                        // Catch the IOException that is raised if the pipe is 
+                        // broken or disconnected.
+                    catch (IOException e)
+                    {
+                        Console.WriteLine("PIPE ERROR: {0}", e.Message);
+                    }
+                    finally
+                    {
+                        if (sw != null)
+                        {
+                            sw.Close();
+                            sw.Dispose();
+                        }
+                        if(sr!=null)
+                        {
+                            sr.Close();
+                            sr.Dispose();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Libraries/IndependentRunner/PipeRunner/RemoteInvoker.ps1
@@ -0,0 +1,19 @@
+﻿[reflection.Assembly]::LoadWithPartialName("system.core")
+$pipe = New-Object system.IO.Pipes.NamedPipeClientStream(".","runnerPipe", [System.IO.Pipes.PipeDirection]"InOut") #, PipeDirection.InOut)
+$pipe.Connect()
+$sw = New-Object System.IO.StreamWriter($pipe)
+$sw.AutoFlush = true;
+$sw.WriteLine("cmd.exe");
+$sw.WriteLine("");
+$sw.Flush();
+$sr = New-Object System.IO.StreamReader($pipe)
+$line = $sr.ReadLine();
+
+$sw.Close();
+$sw.Dispose();
+$sr.Close();
+$sr.Dispose();
+$pipe.Close();
+$pipe.Dispose();
+
+exit $line
