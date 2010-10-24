ade3daab5a05090dde85ad055b132368709d862e
Piotr Jessa <dzesse@gmail.com>
2010-10-24 17:09:31 +0200

    Refactoring some files

---
+++ Source/Nomad/Updater/AvailableModules.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Nomad.Modules.Manifest;
+
+namespace Nomad.Updater
+{
+    /// <summary>
+    /// Complex class that represents available modules list. 
+    /// </summary>
+    /// <remarks>
+    ///     Class is defined this way because of the <see cref="DataContract"/> constraints
+    /// </remarks>
+    [DataContract]
+    public class AvailableModules
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public AvailableModules()
+        {
+        }
+
+
+        /// <summary>
+        ///     Initializes the instance of <see cref="AvailableModules"/> class.
+        /// </summary>
+        /// <param name="manifests">List of <see cref="ModuleManifest"/> </param>
+        public AvailableModules(List<ModuleManifest> manifests)
+        {
+            Manifests = manifests;
+        }
+
+
+        /// <summary>
+        /// List of available modules within repository.
+        /// </summary>
+        [DataMember]
+        public List<ModuleManifest> Manifests { get; set; }
+    }
+}
--- Source/Nomad/Updater/IModulesRepository.cs
+++ Source/Nomad/Updater/IModulesRepository.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Runtime.Serialization;
 using System.ServiceModel;
 using Nomad.Modules.Manifest;
 
@@ -38,62 +37,5 @@ namespace Nomad.Updater
         /// <returns>True if addition was succesful.</returns>
         [OperationContract]
         bool AddModule(ModulePackage module);
-    }
-
-    /// <summary>
-    /// Complex class that represents available modules list. 
-    /// </summary>
-    /// <remarks>
-    ///     Class is defined this way because of the <see cref="DataContract"/> constraints
-    /// </remarks>
-    [DataContract]
-    public class AvailableModules
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        public AvailableModules()
-        {
-            
-        }
-
-        /// <summary>
-        ///     Initializes the instance of <see cref="AvailableModules"/> class.
-        /// </summary>
-        /// <param name="manifests">List of <see cref="ModuleManifest"/> </param>
-        public AvailableModules(List<ModuleManifest> manifests)
-        {
-            Manifests = manifests;
-        }
-
-
-        /// <summary>
-        /// List of available modules within repository.
-        /// </summary>
-        [DataMember]
-        public List<ModuleManifest> Manifests { get; set; }
-    }
-
-
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
     }
 }
