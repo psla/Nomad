0d435523677d810f7e56e10f9fecc3eb46abcca9
Piotr Jessa <dzesse@gmail.com>
2010-12-21 11:55:35 +0100

    Application stubs

---
+++ Source/Nomad.Examples/Application_WPF_Shell/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Nomad.Communication.EventAggregation;
+using Nomad.Communication.ServiceLocation;
+
+namespace Application_WPF_Shell
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow(IServiceLocator locator, IEventAggregator aggregator, ManualResetEvent resetEvent)
+        {
+            InitializeComponent();
+
+            
+        }
+    }
+}
---
+++ Source/Nomad.Examples/Application_WPF_Shell/ShellBootstraper.cs
@@ -0,0 +1,68 @@
+using System.Threading;
+using Nomad.Communication.EventAggregation;
+using Nomad.Communication.ServiceLocation;
+using Nomad.Modules;
+using Nomad.Regions;
+using Nomad.Regions.Adapters;
+
+namespace Application_WPF_Shell
+{
+    public class ShellBootstraper :  IModuleBootstraper
+    {
+        private readonly IServiceLocator _locator;
+        private readonly IEventAggregator _aggregator;
+
+        private readonly ManualResetEvent _resetEvent = new ManualResetEvent(false);
+        
+        private App _app;
+        private Thread _guiThread;
+        
+        
+        public ShellBootstraper(IServiceLocator locator, IEventAggregator aggregator)
+        {
+            _locator = locator;
+            _aggregator = aggregator;
+        }
+
+        #region IModuleBootstraper Members
+
+        /// <summary>
+        ///     Start WPF shell
+        /// </summary>
+        public void OnLoad()
+        {
+            _guiThread = new Thread(StartApplication);
+            _guiThread.SetApartmentState(ApartmentState.STA);
+            _guiThread.Start();
+            _resetEvent.WaitOne();
+        }
+
+        private void StartApplication()
+        {
+            _app = new App();
+            var guiThreadProvider = (new WpfGuiThreadProvider(_app.Dispatcher));
+            RegionManager regionManager = null;
+            guiThreadProvider.RunInGui((ThreadStart) delegate
+                                                         {
+                                                             regionManager = new RegionManager(null);
+                                                         });
+
+            _locator.Register<IGuiThreadProvider>(guiThreadProvider);
+            _locator.Register(regionManager);
+            
+            _app.Run(new MainWindow(_locator, _aggregator, _resetEvent));
+        }
+
+
+        /// <summary>
+        ///         Kill all threads when unload. Shutdown the application
+        /// </summary>
+        public void OnUnLoad()
+        {
+            // when killing appdomain thread.abort is allowed :]
+            _guiThread.Abort();
+        }
+
+        #endregion
+    }
+}
