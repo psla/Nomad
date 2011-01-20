63251df7c2bc2f411620f306eeac64967dba35d6
Mikolaj Dobski <ankhazam@gmail.com>
2011-01-20 20:30:40 +0100

    Psla wanted a commit.

--- Source/Nomad.Examples/Application_Thin_Host/Program.cs
+++ Source/Nomad.Examples/Application_Thin_Host/Program.cs
@@ -16,6 +16,7 @@ namespace Application_Thin_Host
             // load the Application_WPF_Shell
             kernel.LoadModules(new SingleModuleDiscovery(@".\Application_WPF_Shell\Application_WPF_Shell.exe"));
 
+
             kernel.LoadModules(new CompositeModuleDiscovery(
                 new SingleModuleDiscovery(@".\FileLoaderModule\FileLoaderModule.dll"),
                 new SingleModuleDiscovery(@".\GraphicFilesHandlerModule\GraphicFilesHandlerModule.dll"),
